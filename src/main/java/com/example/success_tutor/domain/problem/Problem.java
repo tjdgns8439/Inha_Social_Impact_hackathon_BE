c6084afa9de5f7f992a107426391913326378777
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-05 23:37:25 +0900

    연관관계 세팅

--- src/main/java/com/example/success_tutor/domain/problem/Problem.java
+++ src/main/java/com/example/success_tutor/domain/problem/Problem.java
@@ -1,11 +1,14 @@
 package com.example.success_tutor.domain.problem;
 
+import com.example.success_tutor.domain.reply.Reply;
 import com.example.success_tutor.domain.student.Student;
 import com.example.success_tutor.global.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -23,8 +26,12 @@ public class Problem extends BaseEntity {
     @Column(name = "picture")
     private String picture;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "studentId")
     private Student student;
+
+    @OneToMany(mappedBy = "problem")
+    private List<Reply> replies;
+
 
 }
