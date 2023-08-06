fa975df0b997ceee6bf18ddd6de4c0318cebc8d0
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-06 11:58:15 +0900

    Update ProblemService.java

--- src/main/java/com/example/success_tutor/domain/problem/ProblemService.java
+++ src/main/java/com/example/success_tutor/domain/problem/ProblemService.java
@@ -1,14 +1,39 @@
 package com.example.success_tutor.domain.problem;
 
+import com.example.success_tutor.domain.problem.dto.ProblemRequestDto;
+import com.example.success_tutor.domain.problem.dto.ProblemResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProblemService {
     private final ProblemRepository problemRepository;
+
+
+    @Transactional
+    public ProblemResponseDto getProblem(Long problemId) {
+        Optional<Problem> problem = problemRepository.findById(problemId);
+        ProblemResponseDto responseDto = new ProblemResponseDto();
+        BeanUtils.copyProperties(problem, responseDto);
+        return responseDto;
+    }
+
+    @Transactional
+    public Problem postProblem(ProblemRequestDto dto) {
+        Problem problem = Problem.toProblem(dto);
+        return problemRepository.save(problem);
+    }
+
+
+    @Transactional
+    public void deleteProblem(Long id) {
+        problemRepository.deleteById(id);
+    }
 
 }
