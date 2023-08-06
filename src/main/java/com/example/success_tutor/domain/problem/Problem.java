b6fad05eca02e54d57cbf44bfad89ed186cfbac6
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-06 11:57:32 +0900

    Update Problem.java
    - add toProblem

--- src/main/java/com/example/success_tutor/domain/problem/Problem.java
+++ src/main/java/com/example/success_tutor/domain/problem/Problem.java
@@ -1,15 +1,18 @@
 package com.example.success_tutor.domain.problem;
 
+import com.example.success_tutor.domain.problem.dto.ProblemRequestDto;
 import com.example.success_tutor.domain.reply.Reply;
 import com.example.success_tutor.domain.student.Student;
 import com.example.success_tutor.global.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Builder
 @Getter
 @Entity
 @AllArgsConstructor
@@ -33,5 +36,11 @@ public class Problem extends BaseEntity {
     @OneToMany(mappedBy = "problem")
     private List<Reply> replies;
 
+    public static Problem toProblem(ProblemRequestDto dto){
+        return Problem.builder()
+                .content(dto.getContent())
+                .picture(dto.getPicture())
+                .build();
+    }
 
 }
