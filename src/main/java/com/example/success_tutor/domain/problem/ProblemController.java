6d2e36ddb5ffb7cfd0803ebdd7db365b2846fd60
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-06 11:57:46 +0900

    Update ProblemController.java

--- src/main/java/com/example/success_tutor/domain/problem/ProblemController.java
+++ src/main/java/com/example/success_tutor/domain/problem/ProblemController.java
@@ -1,12 +1,13 @@
 package com.example.success_tutor.domain.problem;
 
+import com.example.success_tutor.domain.problem.dto.ProblemRequestDto;
+import com.example.success_tutor.domain.problem.dto.ProblemResponseDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,12 +16,21 @@ import java.util.List;
 @RequestMapping("/problem")
 public class ProblemController {
 
-    @Autowired
-    ProblemRepository problemRepository;
+    private final ProblemService problemService;
 
-    @GetMapping("/list")
-    public List<Problem> list(){
-        return problemRepository.findAll();
+    @PostMapping("/")// 경로를 지정하지 않음
+    public ResponseEntity<?> postProblem(@RequestBody ProblemRequestDto requestDto) {
+        return new ResponseEntity<>(problemService.postProblem(requestDto), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProblem(@PathVariable Long id) {
+        return new ResponseEntity<>(problemService.getProblem(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProblem(@PathVariable("id") Long id) {
+        problemService.deleteProblem(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
