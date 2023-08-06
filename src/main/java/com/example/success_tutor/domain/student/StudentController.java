d30e301765475fa216b839370753fb79f381439c
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-06 11:58:06 +0900

    Update StudentController.java

--- src/main/java/com/example/success_tutor/domain/student/StudentController.java
+++ src/main/java/com/example/success_tutor/domain/student/StudentController.java
@@ -1,4 +1,15 @@
 package com.example.success_tutor.domain.student;
 
+import com.example.success_tutor.global.BaseEntity;
+import com.sun.source.tree.MemberSelectTree;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+    private final StudentRepository studentRepository;
+
 }
