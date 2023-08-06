2556915a74c38fc1d6b3fd3571ce665ca599edc4
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-06 11:56:58 +0900

    create problemDto
    - create ProblemResponseDto
    - create ProblemRequestDto

---
+++ src/main/java/com/example/success_tutor/domain/problem/dto/ProblemRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.success_tutor.domain.problem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProblemRequestDto {
+
+    private String content;
+    private String picture;
+    private Long studentId;
+}
