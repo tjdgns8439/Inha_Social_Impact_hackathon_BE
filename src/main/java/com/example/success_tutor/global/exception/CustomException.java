d4cd9a682d9be5835857ba18ccd45605d695887e
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-10 21:23:11 +0900

    Create CustomException.java
    - CustomException클래스 생성
    - Runtime에 발생하는 에러 관리

---
+++ src/main/java/com/example/success_tutor/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.success_tutor.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
