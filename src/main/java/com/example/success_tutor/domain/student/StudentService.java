cb15ecd0cca0031ae8d6a76702f7b3ba8b189d8e
tjdgns8439 <tjdgns8439@inha.edu>
2023-08-10 21:24:35 +0900

    Update StudentService.java
    - 가입 할 때 폰번호가 이미 저장되어 있으면 리퍼지토리에 저장하지 않고 예외처리

--- src/main/java/com/example/success_tutor/domain/student/StudentService.java
+++ src/main/java/com/example/success_tutor/domain/student/StudentService.java
@@ -2,6 +2,7 @@ package com.example.success_tutor.domain.student;
 
 import com.example.success_tutor.domain.student.dto.CreateStudentRequestDto;
 import com.example.success_tutor.domain.student.dto.StudentResponseDto;
+import com.example.success_tutor.global.exception.student.StudentPhoneAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,10 @@ public class StudentService {
 
     @Transactional
     public void createStudent(CreateStudentRequestDto dto) {
+        String phoneNum = dto.getPhoneNum();  // 학생 생성 요청에서 핸드폰 번호 가져옴
+        if (studentRepository.existsByPhoneNum(phoneNum)) {
+            throw new StudentPhoneAlreadyExistsException();  // 이미 등록된 번호인 경우 예외 발생
+        }
         studentRepository.save(Student.toEntity(dto));
     }
 
